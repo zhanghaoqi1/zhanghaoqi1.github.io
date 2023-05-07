11bf321d33d75ee7a1fe7254f45484116e4881a6
zhanghaoqi1 <489576803@qq.com>
2023-05-07 15:43:22 +0800

    Site updated: 2023-05-07 15:43:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/03/Counter-Strike的下个时代/","2023/04/29/hello-world/","2023/04/30/我们的60华里远足/","2023/05/04/G2-Announces-Swanis-Coach-Job-Confirmation/","2023/05/03/Markdown语法与外挂标签写法汇总/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/03/Counter-Strike的下个时代/","2023/05/04/G2-Announces-Swanis-Coach-Job-Confirmation/","2023/04/29/hello-world/","2023/05/03/Markdown语法与外挂标签写法汇总/","2023/05/07/关于我的博客/","2023/04/30/我们的60华里远足/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
