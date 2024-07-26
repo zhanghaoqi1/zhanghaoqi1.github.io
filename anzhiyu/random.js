2009c04ecf7e4e33c3578f1569e7924f95662f47
zhanghaoqi1 <489576803@qq.com>
2024-07-26 19:37:18 +0800

    Site updated: 2024-07-26 19:37:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/03/Counter-Strike的下个时代/","2023/04/30/我们的60华里远足/","2023/05/04/G2-Announces-Swanis-Coach-Job-Confirmation/","2023/05/03/Markdown语法与外挂标签写法汇总/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/03/Markdown语法与外挂标签写法汇总/","2024/07/26/红米ax6000刷openwrt教程，终于有完善好用的openwrt了/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
